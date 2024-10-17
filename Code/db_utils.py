e2fccb33adc20c2f9273596dbdd0add8fd331ef9
HJ2207 <harshitajangde@gmail.com>
2024-10-17 14:49:55 +0530

    updated injest.py

--- Code/db_utils.py
+++ Code/db_utils.py
@@ -21,6 +21,7 @@
             # SQLAlchemy 2.0.31
 
 from pymongo import MongoClient
+import pandas as pd
 
 def connect_to_mongodb(host, port, db_name):
     # Function to connect to MongoDB using the provided configuration
@@ -32,9 +33,16 @@ def insert_data_to_mongodb(data, collection_name, db):
     # Function to insert data into MongoDB collection
     collection = db[collection_name]
     
-    # Convert DataFrame to dictionary format for MongoDB
-    data_dict = data.to_dict(orient='records')
+    # Check if the input data is a DataFrame (for CSV data)
+    if isinstance(data, pd.DataFrame):
+        # Convert DataFrame to dictionary format for MongoDB
+        data_dict = data.to_dict(orient='records')
+        # Insert data into MongoDB collection
+        collection.insert_many(data_dict)
     
-    # Insert data into MongoDB collection
-    collection.insert_many(data_dict)
+    # If data is not a DataFrame, assume it's a list of image paths
+    elif isinstance(data, list):
+        # Insert each image path into the MongoDB collection
+        for image_path in data:
+            collection.insert_one({"image_path": image_path})
 
